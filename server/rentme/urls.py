434e63bad306926200a6d71b2ee34fdd7b3ab75e
kyalo-alfred <kyalo.alfred@strathmore.edu>
2025-11-11 12:01:52 +0300

    My latest updates before syncing

--- server/rentme/urls.py
+++ server/rentme/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/auth/', include('accounts.urls')),
     path('api/logistics/', include('logistics_demo.urls')),
     path('api/bookings/', include('bookings.urls')),
 ]
